33cb6f1134788ae4c0d20134da7f975e01cad354
oneofthezombies <hunhoekim@gmail.com>
2018-09-13 14:12:09 +0900

    add: 1406 Editor

--- study/study/Stack.h
+++ study/study/Stack.h
@@ -3,44 +3,118 @@
 
 void IronPipe()
 {
-    const char kOpen = '(';
-    const char kClose = ')';
+    //const char kOpen = '(';
+    //const char kClose = ')';
 
-    char last = 0;
-    int count = 0;
-    int result = 0;
+    //char last = 0;
+    //int count = 0;
+    //int result = 0;
 
-    char ch = 0;
-    int numSuccess = 0;
-    while (true)
-    {
-        last = ch;
-        numSuccess = scanf("%c", &ch);
+    //char ch = 0;
+    //int numSuccess = 0;
+    //while (true)
+    //{
+    //    last = ch;
+    //    numSuccess = scanf("%c", &ch);
 
-        if (numSuccess != 1) 
-            break;
+    //    if (numSuccess != 1) 
+    //        break;
 
-        if (ch == '\n')
-            break;
+    //    if (ch == '\n')
+    //        break;
 
-        if (ch == kOpen)
-        {
-            ++count;
-        }
-        else if (ch == kClose)
-        {
-            if (last == kOpen)
-            {
-                --count;
-                result += count;
-            }
-            else if (last == kClose)
-            {
-                --count;
-                result += 1;
-            }
-        }
-    }
+    //    if (ch == kOpen)
+    //    {
+    //        ++count;
+    //    }
+    //    else if (ch == kClose)
+    //    {
+    //        if (last == kOpen)
+    //        {
+    //            --count;
+    //            result += count;
+    //        }
+    //        else if (last == kClose)
+    //        {
+    //            --count;
+    //            result += 1;
+    //        }
+    //    }
+    //}
 
-    printf("%d", result);
+    //printf("%d", result);
+}
+
+void Editor()
+{
+    //int main()
+    //{
+    //    ios_base::sync_with_stdio(false);
+    //    cin.tie(nullptr);
+    //    cout.tie(nullptr);
+
+    //    string text;
+    //    getline(cin, text);
+
+    //    int n;
+    //    cin >> n;
+
+    //    vector<char> before, after;
+
+    //    for (auto c : text)
+    //        before.push_back(c);
+
+    //    for (int i = 0; i < n; ++i)
+    //    {
+    //        char arg0;
+    //        cin >> arg0;
+
+    //        if (arg0 == 'L')
+    //        {
+    //            if (!before.empty())
+    //            {
+    //                after.push_back(before.back());
+    //                before.pop_back();
+    //            }
+    //        }
+    //        else if (arg0 == 'D')
+    //        {
+    //            if (!after.empty())
+    //            {
+    //                before.push_back(after.back());
+    //                after.pop_back();
+    //            }
+    //        }
+    //        else if (arg0 == 'B')
+    //        {
+    //            if (!before.empty())
+    //            {
+    //                before.pop_back();
+    //            }
+    //        }
+    //        else if (arg0 == 'P')
+    //        {
+    //            char arg1;
+    //            cin >> arg1;
+
+    //            before.push_back(arg1);
+    //        }
+    //    }
+
+    //    while (!before.empty())
+    //    {
+    //        after.push_back(before.back());
+    //        before.pop_back();
+    //    }
+
+    //    while (!after.empty())
+    //    {
+    //        cout << after.back();
+    //        after.pop_back();
+    //    }
+
+    //    system("pause");
+
+    //    return 0;
+    //}
 }
