b2845185362f7fbb0c363f346e0b9f3667abd650
oneofthezombies <hunhoekim@gmail.com>
2018-09-13 14:47:10 +0900

    add: 1158 Josepus

---
+++ study/study/Queue.h
@@ -0,0 +1,41 @@
+#pragma once
+
+void Josepus1158()
+{
+//#include <iostream>
+//#include <deque>
+//    using namespace std;
+//
+//    int main()
+//    {
+//        ios_base::sync_with_stdio(false);
+//        cin.tie(nullptr);
+//        cout.tie(nullptr);
+//
+//        int n, m;
+//        cin >> n >> m;
+//
+//        cout << '<';
+//        deque<int> ns(n);
+//        for (int i = 0; i < ns.size(); ++i)
+//            ns[i] = i + 1;
+//
+//        const int n_1 = n - 1;
+//        const int m_1 = m - 1;
+//        for (int ni = 0; ni < n_1; ++ni)
+//        {
+//            for (int mi = 0; mi < m_1; ++mi)
+//            {
+//                ns.push_back(ns.front());
+//                ns.pop_front();
+//            }
+//            cout << ns.front() << ", ";
+//            ns.pop_front();
+//        }
+//        cout << ns.front() << '>';
+//
+//        system("pause");
+//
+//        return 0;
+//    }
+}
