284ca82c4c7d16843cc1c6ea5c25f61116e6594e
oneofthezombies <hunhoekim@gmail.com>
2018-09-13 16:38:55 +0900

    add: ToOne 1463

---
+++ study/study/DynamicProgramming.h
@@ -0,0 +1,56 @@
+#pragma once
+
+void ToOne1463()
+{
+//#include <iostream>
+//#include <vector>
+//    using namespace std;
+//
+//    int ToOne(const int n, vector<int>& d)
+//    {
+//        if (n == 1)
+//            return 0;
+//
+//        if (d[n] > 0)
+//            return d[n];
+//
+//        d[n] = ToOne(n - 1, d) + 1;
+//
+//        if (n % 2 == 0)
+//        {
+//            int temp = ToOne(n / 2, d) + 1;
+//            if (d[n] > temp)
+//                d[n] = temp;
+//        }
+//
+//        if (n % 3 == 0)
+//        {
+//            int temp = ToOne(n / 3, d) + 1;
+//            if (d[n] > temp)
+//                d[n] = temp;
+//        }
+//
+//        return d[n];
+//    }
+//
+//    int main()
+//    {
+//        ios_base::sync_with_stdio(false);
+//        cin.tie(nullptr);
+//        cout.tie(nullptr);
+//
+//        int n;
+//        cin >> n;
+//
+//        vector<int> d(n + 1, 0);
+//
+//        int res = ToOne(n, d);
+//
+//        cout << res;
+//
+//        //system("pause");
+//
+//        return 0;
+//    }
+
+}
