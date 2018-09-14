367b8a39124f7073c8cd6eac86eade8a56c9ecfd
oneofthezombies <hunhoekim@gmail.com>
2018-09-14 15:04:42 +0900

    add: Tiling_11726, Tiling2_11727, AddOneTwoThree_9095 in DynamicProgramming.h

--- study/study/DynamicProgramming.h
+++ study/study/DynamicProgramming.h
@@ -1,6 +1,6 @@
 #pragma once
 
-void ToOne1463()
+void ToOne_1463()
 {
 //#include <iostream>
 //#include <vector>
@@ -53,4 +53,144 @@ void ToOne1463()
 //        return 0;
 //    }
 
+}
+
+void Tiling_11726()
+{
+//#include <iostream>
+//#include <vector>
+//    using namespace std;
+//
+//    using value_t = unsigned long long;
+//
+//    value_t GetNumTiling(const value_t n, vector<value_t>& d)
+//    {
+//        if (d[n] > 0)
+//        {
+//            return d[n];
+//        }
+//
+//        if (n == 0 ||
+//            n == 1)
+//        {
+//            d[n] = 1;
+//            return d[n];
+//        }
+//
+//        const value_t val = GetNumTiling(n - 1, d) + GetNumTiling(n - 2, d);
+//        d[n] = val % 10007;
+//        return d[n];
+//    }
+//
+//    int main()
+//    {
+//        ios_base::sync_with_stdio(false);
+//        cin.tie(nullptr);
+//        cout.tie(nullptr);
+//
+//        value_t n;
+//        cin >> n;
+//
+//        vector<value_t> numTiling(n + 1, 0);
+//
+//        cout << GetNumTiling(n, numTiling);
+//
+//        return 0;
+//    }
+}
+
+void Tiling2_11727()
+{
+//#include <iostream>
+//#include <vector>
+//    using namespace std;
+//
+//    using value_t = unsigned long long;
+//
+//    value_t GetNumTiling(const value_t n, vector<value_t>& d)
+//    {
+//        if (d[n] > 0)
+//        {
+//            return d[n];
+//        }
+//
+//        if (n == 0 ||
+//            n == 1)
+//        {
+//            d[n] = 1;
+//            return d[n];
+//        }
+//
+//        const value_t val = GetNumTiling(n - 1, d) + 2 * GetNumTiling(n - 2, d);
+//        d[n] = val % 10007;
+//        return d[n];
+//    }
+//
+//    int main()
+//    {
+//        ios_base::sync_with_stdio(false);
+//        cin.tie(nullptr);
+//        cout.tie(nullptr);
+//
+//        value_t n;
+//        cin >> n;
+//
+//        vector<value_t> numTiling(n + 1, 0);
+//
+//        cout << GetNumTiling(n, numTiling);
+//
+//        return 0;
+//    }
+}
+
+void AddOneTwoThree_9095()
+{
+//#include <iostream>
+//#include <vector>
+//    using namespace std;
+//
+//    using value_t = int;
+//
+//    value_t GetNumComb(const value_t n, vector<value_t>& d)
+//    {
+//        if (n < 0)
+//        {
+//            return 0;
+//        }
+//
+//        if (d[n] > 0)
+//        {
+//            return d[n];
+//        }
+//
+//        if (n == 0)
+//        {
+//            d[n] = 1;
+//            return d[n];
+//        }
+//
+//        d[n] = GetNumComb(n - 1, d) + GetNumComb(n - 2, d) + GetNumComb(n - 3, d);
+//
+//        return d[n];
+//    }
+//
+//    int main()
+//    {
+//        ios_base::sync_with_stdio(false);
+//        cin.tie(nullptr);
+//        cout.tie(nullptr);
+//
+//        int t;
+//        cin >> t;
+//
+//        for (value_t ti = 0; ti < t; ++ti)
+//        {
+//            int n;
+//            cin >> n;
+//            vector<value_t> d(n + 1, 0);
+//            cout << GetNumComb(n, d) << '\n';
+//        }
+//
+//        return 0;
+//    }
 }
