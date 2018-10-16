8cc91b0252d282c59e774b1ace5bf1a1461a69ee
oneofthezombies <hunhoekim@gmail.com>
2018-10-16 11:04:39 +0900

    add: 10799, 1406

--- study/study/Stack.h
+++ study/study/Stack.h
@@ -263,3 +263,129 @@ void problem9012()
 //        return 0;
 //    }
 }
+
+void problem10799()
+{
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//    using namespace std;
+//
+//    int main()
+//    {
+//        ios::sync_with_stdio(false);
+//        cin.tie(nullptr);
+//        cout.tie(nullptr);
+//
+//        string input;
+//        cin >> ws >> input;
+//
+//        const char open = '(';
+//        const char close = ')';
+//
+//        size_t numPipe = 0;
+//        vector<char> stack;
+//        char before = 0;
+//        for (auto c : input)
+//        {
+//            if (c == open)
+//            {
+//                stack.push_back(c);
+//            }
+//            else if (c == close)
+//            {
+//                if (stack.empty())
+//                {
+//                    //cout << "Â¦ÀÌ ¾È ¸ÂÀ½\n";
+//                    break;
+//                }
+//
+//                if (before == open)
+//                {
+//                    stack.pop_back();
+//
+//                    numPipe += stack.size();
+//                }
+//                else if (before == close)
+//                {
+//                    stack.pop_back();
+//
+//                    numPipe++;
+//                }
+//            }
+//
+//            before = c;
+//        }
+//
+//        cout << numPipe;
+//
+//        return 0;
+//    }
+}
+
+void problem1406()
+{
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//    using namespace std;
+//
+//    using value_t = unsigned int;
+//
+//    int main()
+//    {
+//        ios::sync_with_stdio(false);
+//        cin.tie(nullptr);
+//        cout.tie(nullptr);
+//
+//        string str;
+//        cin >> ws >> str;
+//
+//        vector<char> before(str.begin(), str.end());
+//        vector<char> after;
+//
+//        value_t t = 0;
+//        cin >> t;
+//
+//        for (value_t ti = 0; ti < t; ++ti)
+//        {
+//            char command = 0;
+//            cin >> command;
+//
+//            if (command == 'L')
+//            {
+//                if (false == before.empty())
+//                {
+//                    after.push_back(before.back());
+//                    before.pop_back();
+//                }
+//            }
+//            else if (command == 'D')
+//            {
+//                if (false == after.empty())
+//                {
+//                    before.push_back(after.back());
+//                    after.pop_back();
+//                }
+//            }
+//            else if (command == 'B')
+//            {
+//                if (false == before.empty())
+//                {
+//                    before.pop_back();
+//                }
+//            }
+//            else if (command == 'P')
+//            {
+//                char ch = 0;
+//                cin >> ch;
+//
+//                before.push_back(ch);
+//            }
+//        }
+//
+//        cout << string(before.begin(), before.end()) << string(after.rbegin(), after.rend());
+//
+//        return 0;
+//    }
+}
