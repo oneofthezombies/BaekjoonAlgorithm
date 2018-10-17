2cd464335bda6ee8de266ec8cfd74f965dc1c824
oneofthezombies <hunhoekim@gmail.com>
2018-10-17 11:00:56 +0900

    add: 10845, 1158

--- study/study/Queue.h
+++ study/study/Queue.h
@@ -39,3 +39,128 @@ void Josepus1158()
 //        return 0;
 //    }
 }
+
+void problem10845()
+{
+//#include <iostream>
+//#include <deque>
+//#include <string>
+//    using namespace std;
+//
+//    using value_t = unsigned int;
+//
+//    int main()
+//    {
+//        ios::sync_with_stdio(false);
+//        cin.tie(nullptr);
+//        cout.tie(nullptr);
+//
+//        value_t n = 0;
+//        cin >> n;
+//
+//        deque<value_t> queue;
+//
+//        string cmd = "";
+//        value_t arg = 0;
+//        for (value_t ni = 0; ni < n; ++ni)
+//        {
+//            cin >> ws >> cmd;
+//
+//            if (cmd == "push")
+//            {
+//                cin >> arg;
+//
+//                queue.push_back(arg);
+//            }
+//            else if (cmd == "pop")
+//            {
+//                if (queue.empty())
+//                {
+//                    cout << -1 << '\n';
+//                }
+//                else
+//                {
+//                    cout << queue.front() << '\n';
+//                    queue.pop_front();
+//                }
+//            }
+//            else if (cmd == "size")
+//            {
+//                cout << queue.size() << '\n';
+//            }
+//            else if (cmd == "empty")
+//            {
+//                cout << queue.empty() << '\n';
+//            }
+//            else if (cmd == "front")
+//            {
+//                if (queue.empty())
+//                {
+//                    cout << -1 << '\n';
+//                }
+//                else
+//                {
+//                    cout << queue.front() << '\n';
+//                }
+//            }
+//            else if (cmd == "back")
+//            {
+//                if (queue.empty())
+//                {
+//                    cout << -1 << '\n';
+//                }
+//                else
+//                {
+//                    cout << queue.back() << '\n';
+//                }
+//            }
+//        }
+//
+//        return 0;
+//    }
+}
+
+void problem1158()
+{
+//#include <iostream>
+//#include <deque>
+//#include <string>
+//    using namespace std;
+//
+//    using value_t = unsigned int;
+//
+//    int main()
+//    {
+//        ios::sync_with_stdio(false);
+//        cin.tie(nullptr);
+//        cout.tie(nullptr);
+//
+//        value_t n = 0;
+//        cin >> n;
+//
+//        value_t m = 0;
+//        cin >> m;
+//
+//        deque<value_t> queue(n);
+//        for (value_t ni = 0; ni < n; ++ni)
+//            queue[ni] = ni + 1;
+//
+//        const value_t m_minus_1 = m - 1;
+//        cout << '<';
+//        while (queue.size() > 1)
+//        {
+//            for (value_t i = 0; i < m_minus_1; ++i)
+//            {
+//                queue.push_back(queue.front());
+//                queue.pop_front();
+//            }
+//
+//            cout << queue.front() << ", ";
+//            queue.pop_front();
+//        }
+//
+//        cout << queue.front() << '>';
+//
+//        return 0;
+//    }
+}
