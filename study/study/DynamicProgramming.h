1c0e7135cdcb9848032b50d653e036779b951b38
oneofthezombies <hunhoekim@gmail.com>
2018-10-25 13:16:51 +0900

    add: solution 11727, 9095

--- study/study/DynamicProgramming.h
+++ study/study/DynamicProgramming.h
@@ -873,3 +873,82 @@ void problem11726_2()
 //    }
 
 }
+
+void problem11727_2()
+{
+//#include <cstdio>
+//#include <vector>
+//    using namespace std;
+//
+//    using value_t = unsigned int;
+//    vector<value_t> d;
+//
+//    int main()
+//    {
+//        value_t n = 0;
+//        scanf("%u", &n);
+//
+//        d.resize(n + 1, 0);
+//
+//        d[1] = 1;
+//        d[2] = 3;
+//
+//        for (value_t i = 3; i <= n; ++i)
+//        {
+//            d[i] = d[i - 1] + d[i - 2] * 2;
+//            d[i] %= 10007;
+//        }
+//
+//        printf("%u", d[n]);
+//
+//        return 0;
+//    }
+
+}
+
+void problem9095_2()
+{
+//#include <cstdio>
+//#include <vector>
+//    using namespace std;
+//
+//    using value_t = unsigned int;
+//    vector<value_t> d;
+//
+//    int main()
+//    {
+//        d.resize(4, 0);
+//        d[1] = 1;
+//        d[2] = 2;
+//        d[3] = 4;
+//        value_t last = 3;
+//
+//        value_t t = 0;
+//        scanf("%u", &t);
+//
+//        while (t-- > 0)
+//        {
+//            value_t n = 0;
+//            scanf("%u", &n);
+//
+//            if (n <= last)
+//            {
+//                printf("%u\n", d[n]);
+//                continue;
+//            }
+//
+//            d.resize(n + 1, 0);
+//
+//            for (value_t i = last + 1; i <= n; ++i)
+//            {
+//                d[i] = d[i - 3] + d[i - 2] + d[i - 1];
+//                d[i] %= 10007;
+//            }
+//
+//            printf("%u\n", d[n]);
+//        }
+//
+//        return 0;
+//    }
+
+}
